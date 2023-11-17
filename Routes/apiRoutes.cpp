09c7db331ca10a34c423925607c7c4ccba79855f
L-sampson <lavonsampson@google.com>
2023-11-17 10:01:23 -0500

    Added nlohmann library to parse url data

--- Routes/apiRoutes.cpp
+++ Routes/apiRoutes.cpp
@@ -1,27 +1,71 @@
-#include<iostream>
-#include<curl/curl.h>
-
+#include <iostream>
+#include <curl/curl.h>
+#include <string.h>
+#include <nlohmann/json.hpp>
+#include <vector>
+#define base_url "https://api.the-odds-api.com/v4/sports/?apiKey=fd47356e9ba1963492996e83733444db"
 
 using namespace std;
-//creating Api routes in the folder
-int main (){
-    // string API_KEY = "fd47356e9ba1963492996e83733444db";
-    // string url ="https://api.the-odds-api.com/v4/sports/?apiKey=";
-    // //concatenate the string to add API_KEY.
-    // string apiURL = url.append(API_KEY);
-    
+using namespace nlohmann;
+// creating Api routes in the folder
+
+void parse(string &readBuff){
+    auto p = json::parse(readBuff);
+    json pdata = p;
+    cout<<pdata[0];
+}
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((string *)userp)->append((char *)contents, size * nmemb);
+    return size * nmemb;
+}
+
+// GET methods:
+void getSports(char buff[])
+{
+    string readBuff;
     CURL *sports = curl_easy_init();
     CURLcode res;
     curl_easy_setopt(sports, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_easy_setopt(sports, CURLOPT_URL, "https://api.the-odds-api.com/v4/sports/?apiKey=fd47356e9ba1963492996e83733444db");
+    curl_easy_setopt(sports, CURLOPT_URL, buff);
+    curl_easy_setopt(sports, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(sports, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(sports, CURLOPT_WRITEDATA, &readBuff);
 
     res = curl_easy_perform(sports);
-    if(res != CURLE_OK){
-        cout<<"Error downloading web page: "<<curl_easy_strerror(res)<<endl;
+    if (res != CURLE_OK)
+    {
+        cout << "Error downloading web page: " << curl_easy_strerror(res) << endl;
         curl_easy_cleanup(sports);
-        return 1;
     }
     curl_easy_cleanup(sports);
-    return 0;
+    cout << "\nSuccess\n";
+    // return readBuff;
+    //fix
+    parse(readBuff);
+}
 
+int main()
+{
+    string url = base_url;
+    vector<char> getUrl(url.begin(), url.end());
+    char buffer[getUrl.size() + 1];
+    // Copy the vector contents to the buffer
+    std::copy(getUrl.begin(), getUrl.end(), buffer);
+
+    // Add the null terminator
+    buffer[getUrl.size()] = '\0';
+
+    getSports(buffer);
+    
+    
+    // Pass the buffer to the function
+    // getSports(buffer);
+    // strlen(url);
+    // strlen(api_key);
+    // strcat(url, api_key);
+    // url = url.append(api_key);
+    // cout<<url<<endl;
+    //getSports(getUrl);
 }

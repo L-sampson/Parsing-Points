1d920e1b2459c818ec89dd148b45aafc8c6128a9
L-sampson <lavonsampson@google.com>
2024-07-19 15:56:49 -0400

    Added basic Angular front-end to communicate with C++ backend

---
+++ Client/ParsingPointsClient/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ParsingPointsClient';
+  data: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('http://0.0.0.0:8080/sports')
+    .subscribe(data => {
+      this.data = data;
+      data.forEach((element: any) => {
+        console.log(element);
+        console.log("Title: ", element.title)
+      });
+    })
+  }
+}
--- Server/data/curl_request.hpp
+++ Server/data/curl_request.hpp
@@ -1,7 +1,18 @@
+#ifndef CURL_REQUEST_HPP
+#define CURL_REQUEST_HPP
+
+
 #include <iostream>
 #include <string>
 #include <curl/curl.h>
 
-extern size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
+class CurlRequest {
+    public:
+    CURLcode httpRequest(const char *view, std::string &response);
 
-CURLcode httpRequest(const char *view, std::string &response);
+    private:
+    static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
+    CURL* curl;
+};
+
+#endif // CURL_REQUEST_HPP

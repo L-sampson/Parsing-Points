0450d041dffb500398929c4b47fd027e8da0fd90
L-sampson <lavonsampson@google.com>
2024-08-03 08:39:00 -0400

    Added styling and carousel

--- Client/ParsingPointsClient/src/app/app.component.ts
+++ Client/ParsingPointsClient/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { SportsListComponent } from './components/sports-list/sports-list.component';
+import { SportsCarouselComponent } from './components/sports-carousel/sports-carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HttpClientModule, HeaderComponent, SportsListComponent],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, HeaderComponent, SportsListComponent, SportsCarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

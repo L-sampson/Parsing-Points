e87d407ae313d636c931695784cc1a59b6e2d364
L-sampson <lavontech@gmail.com>
2024-08-05 21:56:48 -0400

    Added nginx file to config proxy requests to backend server

---
+++ Client/ParsingPointsClient/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import { Environment } from "./environment-definition";
+
+export const environment: Environment = {
+    production: true,
+    API_URL: 'https://parsing-server-e2gdd0g5bycahwgd.eastus-01.azurewebsites.net/api',
+  };
